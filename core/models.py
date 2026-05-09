5a74c1acc0e25f9b8b1a9978bb307e95e77cd56c
khidrhussein <husseinkhidr3@gmail.com>
2026-05-09 15:50:59 +0100

    Change OTP wording to 'verification code'

--- core/models.py
+++ core/models.py
@@ -182,7 +182,7 @@ class OTPManager:
         )
         
         # Send OTP via SMS
-        message = f"Your Paypadi security code is: {otp_code}. Valid for {expiry_minutes} minutes."
+        message = f"Your Paypadi verification code is: {otp_code}. Valid for {expiry_minutes} minutes."
         send_sms(phone_number, message)
         
         return otp
